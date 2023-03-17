90196c05ca7d70d125fde1bd57fd05596e6c1b50
Matheus Domingues <mt-domingues@hotmail.com>
2023-03-17 12:11:19 -0300

    feat: apply voucher test unit suite

--- tests/unit/voucherService.test.ts
+++ tests/unit/voucherService.test.ts
@@ -49,38 +49,106 @@ describe("Create voucher unit test suite", () => {
     }); 
 })
 
-// describe("Apply voucher unit test suite", () => {
-// // it("should apply the voucher", async () => {
-// //     const voucher = {code: generateCode(),discount: generateDiscount(),  used: false};
-// //     const usedVoucher = {...voucher, used: true}
-// //     const amount = 152;
-// //     const finalAmount = amount - (amount * (voucher.discount / 100));
+describe("Apply voucher unit test suite", () => {
 
-// //     jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce((): any => {return [voucher]});
-// //     jest.spyOn(voucherRepository, "useVoucher").mockImplementationOnce((): any => {return [usedVoucher]});
+    it("should apply the voucher", async () => {
+        
+    jest
+        .spyOn(voucherRepository, "getVoucherByCode")
+        .mockImplementationOnce((code): any => {
+            return {
+                id: 1,
+                code: voucher.code,
+                discount: voucher.discount,
+                used: false
+            };
+        });
 
-// //     const result = await voucherService.applyVoucher(voucher.code, amount);
+    jest
+        .spyOn(voucherRepository, "useVoucher")
+        .mockImplementationOnce((): any => {return});
+    
+    const response = await voucherService.applyVoucher(voucher.code, 100);
+     
+    let expectedResult = {
+         amount: 100,
+         discount: voucher.discount,
+         finalAmount: 100 - (100 * (voucher.discount / 100)),
+         applied: true
+    }
 
-// //     console.log(result)
-///
-// //     let expectedResult = {
-// //         amount: amount,
-// //         discount: voucher.discount,
-// //         finalAmount: finalAmount,
-// //         applied: finalAmount !== amount
-// //     }
+     expect(response).toEqual(expectedResult);
+    });
 
-// //     expect(result).toBe(expectedResult);
-// // });
+    it("should respond with 'Voucher does not exist.' ", async () => {
 
-// // it("should respond with 'Voucher does not exist.' ", () => {
+        jest
+        .spyOn(voucherRepository, "getVoucherByCode")
+        .mockImplementationOnce((): any => {
+            return false;
+        });
 
-// // });
+        const response = voucherService.applyVoucher(voucher.code, 100);
+        expect(response).rejects.toEqual({message: "Voucher does not exist.", type: "conflict"})
+    });
+   
+   it("the minimun amount is invalid for discount",async () => {
 
-// // it("amount is invalid for discount", () => {
+    jest
+        .spyOn(voucherRepository, "getVoucherByCode")
+        .mockImplementationOnce((code): any => {
+            return {
+                id: 1,
+                code: voucher.code,
+                discount: voucher.discount,
+                used: false
+            };
+        });
 
-// // });
-// // it("voucher is already used", () => {
+    jest
+        .spyOn(voucherRepository, "useVoucher")
+        .mockImplementationOnce((): any => {return});
+    
+    const response = await voucherService.applyVoucher(voucher.code, 99);
+     
+    let expectedResult = {
+         amount: 99,
+         discount: voucher.discount,
+         finalAmount: 99,
+         applied: false
+    };
 
-// // });
-// })
+     expect(response).toEqual(expectedResult);
+    });   
+
+    it("the voucher is already used", async () => {
+        
+        jest
+        .spyOn(voucherRepository, "getVoucherByCode")
+        .mockImplementationOnce((code): any => {
+            return {
+                id: 1,
+                code: voucher.code,
+                discount: voucher.discount,
+                used: true
+            };
+        });
+
+    jest
+        .spyOn(voucherRepository, "useVoucher")
+        .mockImplementationOnce((): any => {return});
+    
+    const response = await voucherService.applyVoucher(voucher.code, 100);
+     
+    let expectedResult = {
+         amount: 100,
+         discount: voucher.discount,
+         finalAmount: 100,
+         applied: false
+    };
+
+     expect(response).toEqual(expectedResult);    
+
+    });
+ 
+});
